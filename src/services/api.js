fa28892532b6cc2dfed426358db10c2334284a21
André Martins <andrebertonhadev@gmail.com>
2019-08-30 19:59:14 -0300

    authorization key config

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+axios.defaults.headers.common['Authorization'] = 'Bearer c8139381-aea0-487b-9609-8685a5624d73';
+
 const api = axios.create({
   baseURL: 'https://api.github.com'
 });
