1c73f33f056298519d84171ddc54c95ac3c3c2c7
André Martins <andrebertonhadev@gmail.com>
2019-08-30 20:23:40 -0300

    Variavel ambiente para api do github

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 axios.defaults.headers.common['Authorization'] = 'Bearer 001899f6-358d-4b4c-8df6-3e8aa8145c7a';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com'
+  baseURL: process.env.API_GIT
 });
 
 module.exports = api;
