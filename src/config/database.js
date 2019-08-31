56118c59afeeb9a3b1e2cb109e2b0489b8b4732c
André Martins <andrebertonhadev@gmail.com>
2019-08-30 21:54:07 -0300

    config: middleware and database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: 'db',
+  host: 'localhost',
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: 'payprev',
--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,14 @@ const CommonGitUserController = require('./controllers/CommonGitUserController')
 const routes = express.Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.get('/users/:login', GitUserController.index);
 routes.post('/user', GitUserController.store);
 
 routes.get('/gitusers', CommonGitUserController.index);
 routes.post('/gitusers/:list', CommonGitUserController.store);
-
-routes.post('/sessions', authMiddleware, SessionController.store);
 
 module.exports = routes;
