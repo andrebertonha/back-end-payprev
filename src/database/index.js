857b670cc95b3a618268cea80df4ea9d963767a9
André Martins <andrebertonhadev@gmail.com>
2019-08-31 15:40:21 -0300

    postgresql node config

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,7 @@ const GitUser = require('../models/GitUser');
 const Tag = require('../models/Tag');
 
 const databaseConfig = require('../config/database');
+const { Client } = require('pg');
 
 const models = [ User, GitUser, Tag ];
 
@@ -14,6 +15,15 @@ class Database {
   }
 
   init() {
+
+    if(process.env.DATABASE_URL) {
+      const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+      });
+      client.connect();
+    }
+
     this.connection =  new Sequelize(databaseConfig);
     models.map(model => model.init(this.connection))
     .map(model => model.associate && model.associate(this.connection.models));
