0aa122757f2cd8db25059f390f1af616b4f65238
André Martins <andrebertonhadev@gmail.com>
2019-08-31 08:54:10 -0300

    change middleware for specific routes

--- src/routes.js
+++ src/routes.js
@@ -13,12 +13,12 @@ routes.get('/', UserController.index);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
 
-routes.get('/users/:login', GitUserController.index);
-routes.post('/user', GitUserController.store);
 
-routes.get('/gitusers', CommonGitUserController.index);
-routes.post('/gitusers/:list', CommonGitUserController.store);
+routes.get('/users/:login', authMiddleware, GitUserController.index);
+routes.post('/user', authMiddleware, GitUserController.store);
+
+routes.get('/gitusers', authMiddleware, CommonGitUserController.index);
+routes.post('/gitusers/:list', authMiddleware, CommonGitUserController.store);
 
 module.exports = routes;
