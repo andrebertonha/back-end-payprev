281fa76cdbe564e5909d2288891290dd7e4359e4
André Martins <andrebertonhadev@gmail.com>
2019-08-31 08:41:07 -0300

    database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,5 @@
 require('dotenv/config');
-const URL_PG = process.env.DATABASE_URL;
 module.exports = {
-
-
-  if (URL_PG) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      logging:  true //false
-    });
-  },
-
   dialect: 'postgres',
   host: 'localhost',
   username: process.env.DB_USERNAME,
