13b55a258df0415099ae42a430f789d866b57321
André Martins <andrebertonhadev@gmail.com>
2019-08-31 16:51:36 -0300

    define env var to github api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 const axios = require('axios');
 
-axios.defaults.headers.common['Authorization'] = 'Bearer 001899f6-358d-4b4c-8df6-3e8aa8145c7a';
-
 const api = axios.create({
   baseURL: process.env.API_GIT
 });
